06b291b8994b226be2ec3b6b23bb89d86b4b6658
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-09-29 00:10:36 -0400

    Add shell script to mount EFS drive on EC2

---
+++ bin/mount_drive.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo "Listing block devices..."
+lsblk
+echo
+
+# the output should be /dev/nvme1n1: data for empty drives
+sudo file -s /dev/nvme1n1
+
+
+if [[ $(sudo file -s /dev/nvme1n1 | grep -L ": data") ]]; then
+	echo "Drive is not empty."
+else
+	echo "Drive is empty. Mounting drive..."
+
+	# format
+	sudo mkfs -t xfs /dev/nvme1n1
+
+	# create a folder in current directory and mount the nvme drive
+	sudo mkdir /data
+	sudo mount /dev/nvme1n1 /data
+	echo
+
+	# change owner and group
+	sudo chown ubuntu /data
+	sudo chgrp ubuntu /data
+fi
+
+
+# check existence
+echo "Checking amount of free disk space available..."
+df -h
+
